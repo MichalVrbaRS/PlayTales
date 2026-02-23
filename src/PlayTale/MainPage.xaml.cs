0d57df1b197402b663e6962feab74c5be9c64ca1
Michal Vrba <m.vrba@retailsoft.sk>
2026-02-23 20:57:57 +0100

    initial app project

---
+++ src/PlayTale/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PlayTale
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/PlayTale/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PlayTale
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
